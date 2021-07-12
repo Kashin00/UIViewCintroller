fb53a42d0f606be9e4eca60f63965125cd41ac11
Matvey <matvexa333@gmail.com>
2021-07-12 10:30:13 +0300

    for review

--- NavigationControllerTask4/NavigationControllerTask4/NavigationControllerTask4/ViewController.swift
+++ NavigationControllerTask4/NavigationControllerTask4/NavigationControllerTask4/ViewController.swift
@@ -11,9 +11,51 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
+    @IBAction func didPressedSimpleAlertButton(_ sender: Any) {
+        let alert = UIAlertController(title: "Sing out?",
+                                      message: "You can always access your content by signing back in",
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Sing out", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    
+    }
+    
+    @IBAction func didPressedSimpleActionSheetButton(_ sender: Any) {
+        let alert = UIAlertController(title: "Title", message: "Please, select an option", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Approve", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Edit", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: nil))
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func didPressedAlertWithDistractiveButton(_ sender: Any) {
+        let alert = UIAlertController(title: "Title", message: "Please, select an option", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Sing out", style: .destructive, handler: nil))
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func didPressedAlertWithMoreButtons(_ sender: Any) {
+        let alert = UIAlertController(title: "Title", message: "Please, select an option", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Sing out", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Destructive", style: .destructive, handler: nil))
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func didPressedAlertWithUITextField(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "title", message: "message", preferredStyle: .alert)
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
 }
 
